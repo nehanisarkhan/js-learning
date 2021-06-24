d1b872b1d9aa2eb6e3269fc8b3332cd571a020b2
neha <nehaen1036@gmail.com>
2021-06-24 15:38:58 +0530

    nested array

---
+++ index3.js
@@ -0,0 +1,13 @@
+let myNestedArray = [
+    
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  
+    [[true,'deep', 6, 7, 1000, 'method'],],
+    
+    [[['deeper', false, true, 'spread', 'array'],],],
+  
+  [[[['mutate', 1327.98, 'splice', 'slice', false],],],],
+  
+    [[[['deepest', 1.3849, 7, '8.4876', 'arbitrary', 'depth']],],],
+    
+  ];
