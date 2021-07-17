1b21840ea6699ca2ea43c891217b981db1578bb3
neha <nehaen1036@gmail.com>
2021-07-18 00:06:54 +0530

    Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements

--- ES6/es1.js
+++ ES6/es1.js
@@ -48,3 +48,15 @@ const LOCAL_FORECAST = {
 
   
 const {today:{low:lowToday , high:highToday}} = LOCAL_FORECAST;
+
+
+//Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a,b,...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
