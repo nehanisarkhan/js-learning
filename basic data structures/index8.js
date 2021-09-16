a9680670e6b28f3c793be13385450b7c27747f3a
Neha Nisar Khan <nehaen1036@gmail.com>
2021-09-16 17:16:25 +0530

    find vowels using regex

--- basic data structures/index8.js
+++ basic data structures/index8.js
@@ -1,12 +1,30 @@
 function filteredArray(arr, elem) {
-    let newArr = [];
-    for(let i=0; i< arr.length; i++){
-       if (arr[i].indexOf(elem) == -1) {
-        //Checks every parameter for the element and if is NOT there continues the code
-        newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
-      }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      //Checks every parameter for the element and if is NOT there continues the code
+      newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
     }
-    return newArr;
   }
-  
-  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 1));
+  return newArr;
+}
+
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    1
+  )
+);
+
+//REGEX
+
+let quoteSample =
+  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+console.log(result.length); //no. of vowels present
