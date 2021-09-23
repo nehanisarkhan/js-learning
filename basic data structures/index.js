0e6c2bf964b287f02f3affebd7c9ae2b453cd6f4
Neha Nisar Khan <nehaen1036@gmail.com>
2021-09-23 12:55:05 +0530

    two sum

--- basic data structures/index.js
+++ basic data structures/index.js
@@ -1,13 +1,30 @@
-function copyMachine(arr, num) {
-    let newArr = [];
-    while (num >= 1) {
-      
-  newArr.push([...arr]);
-     
-      
+// function copyMachine(arr, num) {
+//     let newArr = [];
+//     while (num >= 1) {
+
+//   newArr.push([...arr]);
+
+//     }
+//     num--;
+//     return newArr;
+//   }
+
+//   console.log(copyMachine([true, false, true], 2));
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+        // console.log([i, j]);
+      }
     }
-    num--;
-    return newArr;
   }
-  
-  console.log(copyMachine([true, false, true], 2));
+};
+
+console.log(twoSum([15, 7, 11, 2, 8, 1], 9));
+// twoSum([15, 7, 11, 2, 8, 1], 9);
