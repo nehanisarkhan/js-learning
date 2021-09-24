183c9e7ada4e72c3291daa5eb9e63792e7b5ca20
Neha Nisar Khan <nehaen1036@gmail.com>
2021-09-24 23:26:35 +0530

    search index

--- basic data structures/index3.js
+++ basic data structures/index3.js
@@ -1,13 +1,25 @@
 let myNestedArray = [
-    
-    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
-  
-    [[true,'deep', 6, 7, 1000, 'method'],],
-    
-    [[['deeper', false, true, 'spread', 'array'],],],
-  
-  [[[['mutate', 1327.98, 'splice', 'slice', false],],],],
-  
-    [[[['deepest', 1.3849, 7, '8.4876', 'arbitrary', 'depth']],],],
-    
-  ];
+  ["unshift", false, 1, 2, 3, "complex", "nested"],
+
+  [[true, "deep", 6, 7, 1000, "method"]],
+
+  [[["deeper", false, true, "spread", "array"]]],
+
+  [[[["mutate", 1327.98, "splice", "slice", false]]]],
+
+  [[[["deepest", 1.3849, 7, "8.4876", "arbitrary", "depth"]]]],
+];
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
+// return the index where it would be if it were inserted in order.
+
+var searchInsert = function (nums, target) {
+  for (let i = 0; i <= nums.length; i++) {
+    if (target <= nums[i]) {
+      return i;
+    }
+  }
+  return nums.length;
+};
+
+console.log(searchInsert([1, 2, 3, 5, 6, 8], 7));
