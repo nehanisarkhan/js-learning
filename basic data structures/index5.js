e4da877fa0798dfa1640d8d0004a24af4e09540f
Neha Nisar Khan <nehaen1036@gmail.com>
2021-09-28 20:47:12 +0530

    switch

--- basic data structures/index5.js
+++ basic data structures/index5.js
@@ -1,12 +1,35 @@
 let userActivity = {
-    id: 23894201352,
-    date: 'January 1, 2017',
-    data: {
-      totalUsers: 51,
-      online: 42
-    }
-  };
-  userActivity.data.online=45;
-  
-  
-  console.log(userActivity);
+  id: 23894201352,
+  date: "January 1, 2017",
+  data: {
+    totalUsers: 51,
+    online: 42,
+  },
+};
+userActivity.data.online = 45;
+
+console.log(userActivity);
+
+function sequentialSizes(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+console.log(sequentialSizes(1));
