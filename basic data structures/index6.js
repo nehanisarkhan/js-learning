d4bab035ac6c667a76e6966e110c45cc6a6a4998
Neha Nisar Khan <nehaen1036@gmail.com>
2021-09-29 19:52:07 +0530

    Accesing nested objects

--- basic data structures/index6.js
+++ basic data structures/index6.js
@@ -1,52 +1,69 @@
 let users = {
-    Alan: {
-      age: 27,
-      online: true
-    },
-    Jeff: {
-      age: 32,
-      online: true
-    },
-    Sarah: {
-      age: 48,
-      online: true
-    },
-    Ryan: {
-      age: 19,
-      online: true
-    }
-  };
-  
-  function isEveryoneHere(userObj) {
+  Alan: {
+    age: 27,
+    online: false,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+function isEveryoneHere(userObj) {
   if (
-      userObj.hasOwnProperty("Alan") &&
-      userObj.hasOwnProperty("Jeff") &&
-      userObj.hasOwnProperty("Sarah") &&
-      userObj.hasOwnProperty("Ryan")
-    ) {
-      return true;
-    }
-    return false;
+    userObj.hasOwnProperty("Alan") &&
+    userObj.hasOwnProperty("Jeff") &&
+    userObj.hasOwnProperty("Sarah") &&
+    userObj.hasOwnProperty("Ryan")
+  ) {
+    return true;
   }
+  return false;
+}
 
-  console.log(isEveryoneHere(users));
+console.log(isEveryoneHere(users));
 
-  function countOnline(usersObj) {
-  
-    let result = 0;
-      for (let users in usersObj) {
-        if (usersObj[users].online === true) {
-          result++;
-        }
-      }
-      return result;
+function countOnline(usersObj) {
+  let result = 0;
+  for (let users in usersObj) {
+    if (usersObj[users].online === true) {
+      result++;
     }
-    console.log(countOnline(users))
+  }
+  return result;
+}
+console.log(countOnline(users));
 
+//Generate an Array of All Object Keys with Object.keys()
+function getArrayOfUsers(obj) {
+  return Object.keys(users);
+}
 
-    //Generate an Array of All Object Keys with Object.keys()
-    function getArrayOfUsers(obj) {
-      return Object.keys(users)
-    }
-    
-    console.log(getArrayOfUsers(users));
+console.log(getArrayOfUsers(users));
+
+//Accesing nested objects
+
+var myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+
+var gloveBoxContents = "maps";
+gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
+console.log(myStorage.car.inside["glove box"]);
