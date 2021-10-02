0fa94831c2f95aacd8a6576164d65fffae116c27
Neha Nisar Khan <nehaen1036@gmail.com>
2021-10-02 20:41:36 +0530

    recurssion

--- basic data structures/index7.js
+++ basic data structures/index7.js
@@ -1,27 +1,33 @@
 let user = {
-    name: 'Kenneth',
-    age: 28,
-    data: {
-      username: 'kennethCodesAllDay',
-      joinDate: 'March 26, 2016',
-      organization: 'freeCodeCamp',
-      friends: [
-        'Sam',
-        'Kira',
-        'Tomo'
-      ],
-      location: {
-        city: 'San Francisco',
-        state: 'CA',
-        country: 'USA'
-      }
-    }
-  };
-  
-  function addFriend(userObj, friend) {
+  name: "Kenneth",
+  age: 28,
+  data: {
+    username: "kennethCodesAllDay",
+    joinDate: "March 26, 2016",
+    organization: "freeCodeCamp",
+    friends: ["Sam", "Kira", "Tomo"],
+    location: {
+      city: "San Francisco",
+      state: "CA",
+      country: "USA",
+    },
+  },
+};
+
+function addFriend(userObj, friend) {
   userObj.data.friends.push(friend);
   return userObj.data.friends;
-   
+}
+
+console.log(addFriend(user, "ed"));
+
+//Loops using Recursion
+
+function sum(arr, n) {
+  if (n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
   }
-  
-  console.log(addFriend(user, 'ed'));
+}
+console.log(sum([1], 0));
