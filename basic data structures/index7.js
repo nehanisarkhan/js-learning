3952f4fda086b32ba5e1c6f06a2e957ca808d36f
Neha Nisar Khan <nehaen1036@gmail.com>
2021-10-09 13:42:34 +0530

    factorial

--- basic data structures/index7.js
+++ basic data structures/index7.js
@@ -103,3 +103,39 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello"));
+
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+
+  //Step 4. Return the reversed string
+  return joinArray; // "olleh"
+}
+
+reverseString("hello");
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+reverseString("hello");
+
+//factorial
+
+function factorialize(num) {
+  var fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+  return fact;
+}
+
+factorialize(5);
