74c66a809cc30757a19b84462c2f7bda44638c43
Neha Nisar Khan <nehaen1036@gmail.com>
2021-10-28 01:31:26 +0530

    funtion to return multidimensinal array the size of second argument

--- basic data structures/index4.js
+++ basic data structures/index4.js
@@ -1,14 +1,25 @@
 let foods = {
-    apples: 25,
-    oranges: 32,
-    plums: 28
-  };
-  
-  
-  foods.bananas=13;
-  foods.grapes=35;
-  foods.strawberries=27;
-  foods.apples=30;
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+foods.apples = 30;
 delete foods.plums;
-delete foods.oranges;  
-  console.log(foods);
+delete foods.oranges;
+console.log(foods);
+
+//a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
